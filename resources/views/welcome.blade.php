6adf5f4e40aa87dd3705a0e8ec5ae59c4388b25a
Jadiel dos Santos <jadiel@twel.io>
2020-08-13 00:32:51 -0300

    Testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel FotoPro
+                    Laravel FotoPro Project
                 </div>
 
                 <div class="links">
